07401d4355acc5fd16b9c5b1c7cac8072eefeb36
sujal-patel000 <vaghasiyasujal66@gmail.com>
2024-07-27 07:43:11 +0000

    container config done

---
+++ 01_basics/01_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all js code as newer version
+
+//alert("hello") // we are using node.js note browser
+
+console.log(3 + 3) //code readability should be high
+console.log("sujal")
+
+let name = "sujal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not asign
+// symbol => unique
+//object
+console.log(typeof "sujal")
+console.log(typeof null) // null is object
