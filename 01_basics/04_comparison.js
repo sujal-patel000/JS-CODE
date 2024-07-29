fb77129724caa5e6b51b7eeb7f88b325e958a8a8
sujal-patel000 <vaghasiyasujal66@gmail.com>
2024-07-29 09:35:57 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+console.log(2 == 1)
+console.log(2 >= 1)
+console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null == 0)
+console.log(null >= 0)
+console.log(null > 0)
+
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined > 0)
+
+// === is is strict checking mean is is not only value check but strict check data type also
+console.log("2" === 2)
+// its data type is also different then the answer is false
