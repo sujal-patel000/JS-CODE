38e97e265227da388896fd41bd69a41b8278d46d
sujal-patel000 <vaghasiyasujal66@gmail.com>
2024-07-29 14:54:30 +0000

    Datatype summary

---
+++ 01_basics/Datatypes_summary.js
@@ -0,0 +1,29 @@
+// primitive
+
+// 7 types: string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = 100.3
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol(123)
+const anotherid = Symbol(123)
+console.log(id == anotherid)
+
+const BigNumber = 153726487398394709n
+
+//Reference (non primitive)
+//Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+console.log(heros)
+console.log(typeof heros)
+
+let Myobj = {
+       name: "sujal",
+       age:  20,
+};
+console.log(Myobj)
